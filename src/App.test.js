0236ffab4f278221cc8df125126064e927228f3a
Wes Cravens <wcravens@cortex-it.com>
2022-06-17 17:41:34 -0500

    Finish stripping down the react app project scaffolding.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders the App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Left empty until we learn more about the RTL and Screen.
 });
